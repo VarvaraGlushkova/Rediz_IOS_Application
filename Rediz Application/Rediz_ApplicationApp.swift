cdd87248302780c4cfeb38af4a4f4501592256cc
VarvaraGlushkova <varvara.glushkova@icloud.com>
2025-06-22 13:56:59 +0300

    Initial Commit

---
+++ Rediz Application/Rediz_ApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rediz_ApplicationApp.swift
+//  Rediz Application
+//
+//  Created by Варвара Глушкова on 22.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct Rediz_ApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Rediz ApplicationTests/Rediz_ApplicationTests.swift
@@ -0,0 +1,17 @@
+//
+//  Rediz_ApplicationTests.swift
+//  Rediz ApplicationTests
+//
+//  Created by Варвара Глушкова on 22.06.2025.
+//
+
+import Testing
+@testable import Rediz_Application
+
+struct Rediz_ApplicationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
